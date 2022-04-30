c0b3ed7c4e58affdeb9e29b4b33d430d9458c9ef
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-04-30 10:54:37 -0300

    implementando cache usando o padrão proxy

--- src/main/java/br/com/gabriel/orcamento/Orcamento.java
+++ src/main/java/br/com/gabriel/orcamento/Orcamento.java
@@ -18,6 +18,11 @@ public class Orcamento implements Orcavel {
 
     @Override
     public BigDecimal getValor() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return valor;
     }
 
--- src/main/java/br/com/gabriel/pedido/TestesComposicao.java
+++ src/main/java/br/com/gabriel/pedido/TestesComposicao.java
@@ -2,6 +2,7 @@ package br.com.gabriel.pedido;
 
 import br.com.gabriel.orcamento.ItemOrcamento;
 import br.com.gabriel.orcamento.Orcamento;
+import br.com.gabriel.orcamento.OrcamentoProxy;
 
 import java.math.BigDecimal;
 
@@ -16,6 +17,9 @@ public class TestesComposicao {
         novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
         novo.adicionarItem(antigo);
 
-        System.out.println(novo.getValor());
+        OrcamentoProxy proxy = new OrcamentoProxy(novo);
+
+        System.out.println(proxy.getValor());
+        System.out.println(proxy.getValor());
     }
 }
